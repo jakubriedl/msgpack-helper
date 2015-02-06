23db7fe9f6cb1f8e6f77ca42cd02425ffc1d6e99
Jakub Riedl <jakub.riedl@bileto.cz>
2015-02-06 23:52:17 +0100

    added basic functions

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @param mixed $data
+ * @return string
+ */
+function msgpack_pack($data){
+}
+
+/**
+ * @param string $string
+ * @return array|\StdClass
+ */
+function msgpack_unpack($string){
+}
